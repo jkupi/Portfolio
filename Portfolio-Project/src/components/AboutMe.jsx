e8fd12ab0faa22fd44aea0d997f9794472a8c16c
Jaakob Alakulppi <jbalakulppi@gmail.com>
2024-10-31 17:43:51 -0400

    added contact form functionality

--- Portfolio-Project/src/components/AboutMe.jsx
+++ Portfolio-Project/src/components/AboutMe.jsx
@@ -1,8 +1,13 @@
+import profilePicture from '../assets/profile_pic.jpg'
+
 function AboutMe() {
-    return <div>
-        <h2>About me</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit corrupti iure ullam a quos nobis vitae repudiandae rerum. Deleniti libero aliquid quae assumenda quo eius est aperiam velit aspernatur vel.</p>
-    </div>
+    return <section className='about-me'>
+        <div className='about-me-content'>
+            <h2>About me</h2>
+            <img src={profilePicture} className='profile-picture'/>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit corrupti iure ullam a quos nobis vitae repudiandae rerum. Deleniti libero aliquid quae assumenda quo eius est aperiam velit aspernatur vel. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ea illum nam minus. Quis animi ea sunt blanditiis, reprehenderit nostrum sapiente eligendi enim voluptas itaque, accusamus sed? Facilis odio tenetur rerum?Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed eos labore architecto dignissimos beatae dolorum fugiat saepe fuga, quisquam nesciunt odit pariatur aperiam dicta velit delectus explicabo porro, exercitationem Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta obcaecati tempore ducimus qui aliquam modi, voluptates debitis atque id, dolorum esse beatae numquam unde facere consectetur, quibusdam deserunt veritatis quos!</p>
+        </div>
+    </section>
 }
 
 export default AboutMe;
--- Portfolio-Project/src/components/Contact.jsx
+++ Portfolio-Project/src/components/Contact.jsx
@@ -1,5 +1,89 @@
+import { useState } from "react";
+
 function Contact() {
-    return <div><h2>Contact</h2><p>This is the Contact section.</p></div>;
+  const [formData, setFormData] = useState({ name: '', email: '', message: '' });
+  const [errors, setErrors] = useState({});
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+    setErrors({ ...errors, [name]: '' });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let formIsValid = true;
+    const newErrors = {};
+
+    if (!formData.name.trim()) {
+      newErrors.name = 'name required';
+      formIsValid = false;
+    }
+
+    if (!formData.email.trim()) {
+      newErrors.email = 'email required';
+      formIsValid = false;
+    } else if (!/^\S+@\S+\.\S+$/.test(formData.email)) {
+      newErrors.email = 'enter a valid email address';
+      formIsValid = false;
+    }
+
+    if (!formData.message.trim()) {
+      newErrors.message = 'message required';
+      formIsValid = false;
+    }
+
+    if (!formIsValid) {
+      setErrors(newErrors);
+    } else {
+      alert('Successfull!!!!!!!!');
+      setFormData({ name: '', email: '', message: '' });
+      setErrors({});
+    }
+  }
+
+    return <div className="contact-form">
+        <h2>Contact</h2>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label>Name</label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              value={formData.name}
+              onChange={handleChange}
+            />
+            {errors.name && <span className="error">{errors.name}</span>}
+          </div>
+
+          <div>
+            <label>Email</label>
+            <input 
+              type="email"
+              id="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+            />
+            {errors.email && <span className="error">{errors.email}</span>}
+          </div>
+
+          <div>
+            <label>Message</label>
+            <textarea
+              id="message"
+              name="message"
+              rows="10"
+              value={formData.message}
+              onChange={handleChange}
+              ></textarea>
+              {errors.message && <span className="error">{errors.message}</span>}
+          </div>
+
+          <button type="submit">Submit</button>
+        </form>
+      </div>;
   }
   
   export default Contact;
